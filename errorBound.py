83f14ad7f59cf872b4b4f2a029d3fa1270384a13
Sudiptto Biswas <biswassudiptto@gmail.com>
2024-09-07 15:05:49 -0400

    pushed error bound code

--- errorBound.py
+++ errorBound.py
@@ -1,1 +1,18 @@
 # Check the error bounds 
+
+# storyTitle verification
+# around 5 characters - 100 characters
+def verifyStoryTitle(storyTitle):
+    
+    # if story title all empty than return False
+    if storyTitle.isspace():
+        return False
+
+    # less than 10 characters, excluding spaces
+    if len(storyTitle) < 5:
+        return False
+    # greater than 100
+    if len(storyTitle) > 100:
+        return False
+    
+    return True
